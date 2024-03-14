1343850dba3a46c7bfd8b44b528e967ccb9ea983
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-15 02:54:19 +0800

    Reconfigure vercel.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import * as RecipeAPI from "./recipe-api";
+
+const app = express();
+
+app.use(express.json()); // Converts body of requests & responses to JSON
+app.use(cors()); // Allows for cross-origin requests
+
+app.listen(5000, () => console.log("Server running on localhost:5000"));
+
+app.get("/api/recipes/search", async (req, res) => {
+  console.log(req.query);
+
+  const searchTerm = req.query.searchTerm as string;
+  const page = parseInt(req.query.page as string);
+  const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+  // console.log("Response data:", results);
+  return res.json(results);
+});
+
+export default app;
