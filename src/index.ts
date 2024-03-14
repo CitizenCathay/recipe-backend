94eedd67936e3608f72d1189341a060d5c069321
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-15 04:49:23 +0800

    Reconfigure vercel.json

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,6 @@ const app = express();
 
 app.use(express.json()); // Converts body of requests & responses to JSON
 app.use(cors()); // Allows for cross-origin requests
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on localhost:5000"));
